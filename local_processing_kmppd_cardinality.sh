366a211dc32ed107b0bfa173c230ae4fa3dc2772
mocatfrio <hafarafirdausi@gmail.com>
2021-09-25 15:31:21 +0000

    Edit script sh

--- local_processing_kmppd_cardinality.sh
+++ local_processing_kmppd_cardinality.sh
@@ -7,14 +7,14 @@
 # python3 index.py -o precompute -s 3 -t ind -n 500 -d 2 -m kmppd -g 5
 
 # CARDINALITY
-python3 index.py -o precompute -s 6 -t ind -n 500 -d 3 -m kmppd -g 5
-python3 index.py -o precompute -s 6 -t ind -n 1000 -d 3 -m kmppd -g 5
-python3 index.py -o precompute -s 6 -t ind -n 2000 -d 3 -m kmppd -g 5
-python3 index.py -o precompute -s 6 -t ind -n 5000 -d 3 -m kmppd -g 5
-python3 index.py -o precompute -s 6 -t ind -n 10000 -d 3 -m kmppd -g 5
+#python3 index.py -o precompute -s 6 -t ind -n 500 -d 3 -m kmppd -g 5
+#python3 index.py -o precompute -s 6 -t ind -n 1000 -d 3 -m kmppd -g 5
+#python3 index.py -o precompute -s 6 -t ind -n 2000 -d 3 -m kmppd -g 5
+#python3 index.py -o precompute -s 6 -t ind -n 5000 -d 3 -m kmppd -g 5
+#python3 index.py -o precompute -s 6 -t ind -n 10000 -d 3 -m kmppd -g 5
 
-python3 index.py -o precompute -s 6 -t ant -n 500 -d 3 -m kmppd -g 5
-python3 index.py -o precompute -s 6 -t ant -n 1000 -d 3 -m kmppd -g 5
+#python3 index.py -o precompute -s 6 -t ant -n 500 -d 3 -m kmppd -g 5
+#python3 index.py -o precompute -s 6 -t ant -n 1000 -d 3 -m kmppd -g 5
 python3 index.py -o precompute -s 6 -t ant -n 2000 -d 3 -m kmppd -g 5
 python3 index.py -o precompute -s 6 -t ant -n 5000 -d 3 -m kmppd -g 5
 python3 index.py -o precompute -s 6 -t ant -n 10000 -d 3 -m kmppd -g 5
--- local_processing_optimized_kmppd_cardinality.sh
+++ local_processing_optimized_kmppd_cardinality.sh
@@ -7,10 +7,10 @@
 # python3 index.py -o precompute -s 3 -t ind -n 500 -d 2 -m kmppd -g 5
 
 # CARDINALITY
-python3 index.py -o precompute -s 6 -t ind -n 500 -d 3 -m optimized_kmppd -g 5
-python3 index.py -o precompute -s 6 -t ind -n 1000 -d 3 -m optimized_kmppd -g 5
-python3 index.py -o precompute -s 6 -t ind -n 2000 -d 3 -m optimized_kmppd -g 5
-python3 index.py -o precompute -s 6 -t ind -n 5000 -d 3 -m optimized_kmppd -g 5
+#python3 index.py -o precompute -s 6 -t ind -n 500 -d 3 -m optimized_kmppd -g 5
+#python3 index.py -o precompute -s 6 -t ind -n 1000 -d 3 -m optimized_kmppd -g 5
+#python3 index.py -o precompute -s 6 -t ind -n 2000 -d 3 -m optimized_kmppd -g 5
+#python3 index.py -o precompute -s 6 -t ind -n 5000 -d 3 -m optimized_kmppd -g 5
 python3 index.py -o precompute -s 6 -t ind -n 10000 -d 3 -m optimized_kmppd -g 5
 
 python3 index.py -o precompute -s 6 -t ant -n 500 -d 3 -m optimized_kmppd -g 5
