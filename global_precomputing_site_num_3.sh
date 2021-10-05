3feba915d88ffa454d28baa74e223d4eb46e9b2a
mocatfrio <hafarafirdausi@gmail.com>
2021-10-05 08:21:17 +0000

    Add bash script

---
+++ global_precomputing_site_num_3.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# GLOBAL PROCESSING
+# python3 index.py -o global_precompute -s <site_num> -t <dataset_type> -n <data_num> -d <dim_size> -m <method> -g <grid_size>
+
+# example:
+# python3 index.py -o global_precompute -s 3 -t ind -n 500 -d 2 -m kmppd -g 5
+
+# SITE NUM
+
+python3 index.py -o global_precompute -s 2 -t ant -n 5000 -d 3 -m optimized_kmppd -g 5
+python3 index.py -o global_precompute -s 3 -t ant -n 5000 -d 3 -m optimized_kmppd -g 5
+python3 index.py -o global_precompute -s 4 -t ant -n 5000 -d 3 -m optimized_kmppd -g 5
+python3 index.py -o global_precompute -s 5 -t ant -n 5000 -d 3 -m optimized_kmppd -g 5
+python3 index.py -o global_precompute -s 6 -t ant -n 5000 -d 3 -m optimized_kmppd -g 5
+
+python3 index.py -o global_precompute -s 2 -t fc -n 5000 -d 3 -m optimized_kmppd -g 5
+python3 index.py -o global_precompute -s 3 -t fc -n 5000 -d 3 -m optimized_kmppd -g 5
+python3 index.py -o global_precompute -s 4 -t fc -n 5000 -d 3 -m optimized_kmppd -g 5
+python3 index.py -o global_precompute -s 5 -t fc -n 5000 -d 3 -m optimized_kmppd -g 5
+python3 index.py -o global_precompute -s 6 -t fc -n 5000 -d 3 -m optimized_kmppd -g 5
